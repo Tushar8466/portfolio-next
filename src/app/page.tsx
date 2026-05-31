8c1bc7a2917642baca4df871e6f4afc2592d79c6
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-05-31 17:58:09 +0530

    feat: add BubbleMenu component and integrate it into the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,65 @@
-'use client'
-import { useRef } from 'react'
-import HeroSection from '@/components/HeroSection'
-import ScrollCursor from '@/components/ScrollCursor'
-import TechStack from '@/components/TechStacks'
+"use client";
+import { useRef } from "react";
+import HeroSection from "@/components/HeroSection";
+import ScrollCursor from "@/components/ScrollCursor";
+import TechStack from "@/components/TechStacks";
+import CardNav from "@/components/CardNav";
+import BubbleMenu from "@/components/BubbleMenu";
 
 export default function page() {
-  const heroRef = useRef<HTMLDivElement>(null)
+  const items = [
+  {
+    label: 'home',
+    href: '#',
+    ariaLabel: 'Home',
+    rotation: -8,
+    hoverStyles: { bgColor: '#3b82f6', textColor: '#ffffff' }
+  },
+  {
+    label: 'about',
+    href: '#',
+    ariaLabel: 'About',
+    rotation: 8,
+    hoverStyles: { bgColor: '#10b981', textColor: '#ffffff' }
+  },
+  {
+    label: 'projects',
+    href: '#',
+    ariaLabel: 'Projects',
+    rotation: 8,
+    hoverStyles: { bgColor: '#f59e0b', textColor: '#ffffff' }
+  },
+  {
+    label: 'blog',
+    href: '#',
+    ariaLabel: 'Blog',
+    rotation: 8,
+    hoverStyles: { bgColor: '#ef4444', textColor: '#ffffff' }
+  },
+  {
+    label: 'contact',
+    href: '#',
+    ariaLabel: 'Contact',
+    rotation: -8,
+    hoverStyles: { bgColor: '#8b5cf6', textColor: '#ffffff' }
+  }
+];
+
 
   return (
-    <div style={{ overflowY: 'scroll', height: '100vh' }}>
-        <HeroSection />
-      </div>
-  )
+    <div style={{ overflowY: "scroll", height: "100vh" }}>
+      <BubbleMenu
+        logo={<span style={{ fontWeight: 700 , color: 'black' }}>Tushar Singla</span>}
+        items={items}
+        menuAriaLabel="Toggle navigation"
+        menuBg="#ffffff"
+        menuContentColor="#111111"
+        useFixedPosition={false}
+        animationEase="back.out(1.5)"
+        animationDuration={0.5}
+        staggerDelay={0.12}
+      />
+      <HeroSection />
+    </div>
+  );
 }
